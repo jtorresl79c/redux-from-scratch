1cb46329720e6ecbe8869db6ffe2a5f0b5782828
jtorresl79c <83847060+jtorresl79c@users.noreply.github.com>
2022-06-06 16:39:34 -0700

    6. Writing Clean Redux Code - #7 Creating Reducers

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,4 +1,4 @@
-import { createAction } from "@reduxjs/toolkit"
+import { createAction, createReducer } from "@reduxjs/toolkit"
 
 // Action types
 // const BUG_ADDED = "bugAdded"
@@ -38,6 +38,7 @@ import { createAction } from "@reduxjs/toolkit"
 // Como se dijo arribita, una funcion es un objeto, por lo tanto, puede tener propiedades.
 // la funcion que retorna createAction tiene una propiedad llamada type, que es el nombre de la accion.
 
+// 
 export const bugAdded = createAction("bugAdded") //  { type: "bugAdded", ... }
 
 export const bugRemoved = createAction("bugRemoved") //  { type: "bugRemoved", ... }
@@ -46,29 +47,67 @@ export const bugResolved = createAction("bugResolved") //  { type: "bugResolved"
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Reducer - #1 En el Ducks pattern - El reducer a fuerzas debe de ser el export default
 let lastId = 0
 
-export default function reducer(state = [], action){
+// export default function reducer(state = [], action){
 
-    switch(action.type){
-        case bugAdded.type: // bugAdded es una funcion, pero tiene en su interior la propiedad type el cual es un string, es por lo mismo que no necesitamos llamar a BUG_ADDED (aparte que esta comentado)
-        // porque dentro de bugAddded se encuentra el string que se necesita
-            return [
-                ...state,
-                {
-                    id: ++lastId,
-                    description: action.payload.description,
-                    resolved: false
-                }
-            ]
-        case bugRemoved.type:
-            return state.filter( bug => bug.id !== action.payload.id )
-        case bugResolved.type:
-            return state.map( bug => (bug.id === action.payload.id) ? { ...bug, resolved: true } : bug)
-        default:
-            return state
+//     switch(action.type){
+//         case bugAdded.type: // bugAdded es una funcion, pero tiene en su interior la propiedad type el cual es un string, es por lo mismo que no necesitamos llamar a BUG_ADDED (aparte que esta comentado)
+//         // porque dentro de bugAddded se encuentra el string que se necesita    
+//             return [
+//                 ...state,
+//                 {
+//                     id: ++lastId,
+//                     description: action.payload.description,
+//                     resolved: false
+//                 }
+//             ]
+//         case bugRemoved.type:
+//             return state.filter( bug => bug.id !== action.payload.id )
+//         case bugResolved.type:
+//             return state.map( bug => (bug.id === action.payload.id) ? { ...bug, resolved: true } : bug)
+//         default:
+//             return state
+//     }
+
+
+// }
+
+export default createReducer( [], {
+    // bugAdded: (state, action) => {
+    [bugAdded.type]: (state, action) => {
+        // En react la norma es la inmutabilidad, pero al usar reduxjs/toolkit podemos usar la funcion
+        // createReducer el cual usa ImmerJS, el cual nos ayuda a poder escribir codigo mutable PERO este
+        // de manera automatica se pasa a un esquema de inmutabilidad, casteando el codigo.
+        state.push({
+            id: ++lastId,
+            description: action.payload.description,
+            resolved: false
+        })
+    },
+    // bugResolved: (bugs,action) => {
+    [bugResolved.type]: (bugs,action) => {
+        const index = bugs.findIndex( bug => bug.id === action.payload.id )
+        // Ya que estamos usando el tool kit podemos usar mutabilidad (aunque el compilador de manera automatica
+        // lo hace de manera inmutable)
+        bugs[index].resolved = true
     }
-
-
-}
+} )
