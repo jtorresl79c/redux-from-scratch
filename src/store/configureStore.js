cba68f6abf3144795abcbeddb8dd20731a821c97
jtorresl79c <83847060+jtorresl79c@users.noreply.github.com>
2022-06-06 14:59:34 -0700

    Notas en configureStore.js

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,5 +3,10 @@ import { configureStore } from '@reduxjs/toolkit'
 import reducer from './bugs'
 
 export default function(){
+    // Esto retorna el store, es como si fuera el store = createStore(reducer)
+    
+    // Para usar el Redux Devtools (la extension del navegador) y que funcione, se hacia una 
+    // configuracion algo grande al momento de mandar a llamar al createStore, pero al usar
+    // el configureStore en vez del createStore esa configuracion extra ya no se hace.
     return configureStore({reducer})
 }
